f6451b9112b170d4bae83e2d0c9a9626e72fd41d
chwol <chwollba@gmail.com>
2023-06-20 13:19:52 +0200

    Foundation CAPEX floating & Tripod updated Lower/upper value getrennt aufführen, gemäß Funktionsinput

--- scripts/lcoe.py
+++ scripts/lcoe.py
@@ -1,42 +1,35 @@
 import pandas as pd
 
 
-def calc_lcoe(capacity=1, power_yield=1, distance =1, depth =1, af=1, turbine = 1):
+def calc_lcoe(capacity=1, power_yield=1, distance =1, depth =1, value= "lower"):
 
     tech = pd.read_csv("data/lcoe/tech_data.csv", index_col=0)
 
-    capacity = turbine.capacity ## get from energyharvest
-    power_yield = timeseries.sum()  # get from energyharvest
-    distance = get_distance_to_coast(self)  # get from this function
-    depth = water_depth(depth_map=depth_map, depth=depth_map[row, col]) # get frpm this function
-
     # calculate capex costs
     # Turbine
-    capex_turbine = (tech.loc["Nominal investment (equipment: turbines) [M€/MW_e]"]+tech.loc["Nominal investment (installation: turbines) [M€/MW_e]"])*capacity*1e6
+    capex_turbine = (tech.loc["Nominal investment (equipment: turbines) [M€/MW_e]"][value]+tech.loc["Nominal investment (installation: turbines) [M€/MW_e]"][value])*capacity*1e6
     # Foundation
-    if depth < 20 and distance < 30:    # Monopile
-        capex_found = (tech.loc["Nominal investment (equipment: foundation) [M€/MW_e]"] + tech.loc[
-            "Nominal investment (installation: foundation) [M€/MW_e]"]) * capacity * 1e6
+    if depth < 40 and distance < 30:    # Monopile
+        capex_found = tech.loc["Nominal investment (equipment + installation: foundation monopile) [M€/MW_e]"][value] * capacity * 1e6
 
-    elif (depth > 20 and depth < 30) and (distance > 30 and distance < 90): # Tripod
-        capex_found = 0
-    elif (depth > 30 and depth < 90) and distance < 20: # Floating
-        capex_found = 0
-    else:   # Not suitable for offshore
-        capex_found = 0
+    elif (depth > 40 and depth < 60) and (distance > 30 and distance < 90): # Tripod
+        capex_found = tech.loc["Nominal investment (equipment+installation: foundation Tripod) [M€/MW_e]"][value] * capacity * 1e6
+    else: # Floating
+        capex_found = tech.loc["Nominal investment (equipment+installation: foundation floating) [M€/MW_e]"][value] * capacity * 1e6
+
     # array cables
-    capex_array = (tech.loc["Nominal investment (equipment: array cables) [M€/MW]"]+tech.loc["Nominal investment (installation: array cables) [M€/MW]"])*capacity * 1e6
+    capex_array = (tech.loc["Nominal investment (equipment: array cables) [M€/MW]"][value]+tech.loc["Nominal investment (installation: array cables) [M€/MW]"][value])*capacity * 1e6
     # grid connection
-    capex_export = tech.loc["Nominal investment (equipment+installation: grid connection) [M€/km]"] * distance * 1e6
+    capex_export = tech.loc["Nominal investment (equipment+installation: grid connection) [M€/km]"][value] * distance * 1e6
     # Planning
-    capex_project = tech.loc["Nominal investment (Project development etc.) [M€/MW_e]"]*1e6
+    capex_project = tech.loc["Nominal investment (Project development etc.) [M€/MW_e]"][value]*1e6
     # Sum total capex
     capex = capex_project+capex_export+capex_array+capex_turbine+capex_found
     # calculate annuity factor
-    wacc = tech.loc["WACC_real [%]"] / 100
-    af = wacc / (1 - pow((1 + wacc), -tech.loc["Technical lifetime [years]"]))
+    wacc = tech.loc["WACC_real [%]"][value] / 100
+    af = (wacc * pow((1 + wacc),tech.loc["Technical lifetime [years]"][value]))/(pow((1+wacc),tech.loc["Technical lifetime [years]"][value])-1)
     # calculate OPEX
-    opex = (tech.loc["Fixed O&M [€/MW_e/y]"]*capacity+tech.loc["Variable O&M [€/MWh_e]"]*power_yield)
+    opex = (tech.loc["Fixed O&M [€/MW_e/y]"][value]*capacity+tech.loc["Variable O&M [€/MWh_e]"][value]*power_yield)
     # calculate lcoe
     lcoe = ((capex*af)+opex)/power_yield
 
