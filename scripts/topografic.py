ecc9710d673660538c7c897e1894cf3d6f8f58d0
chwol <chwollba@gmail.com>
2023-06-20 18:14:48 +0200

    Added functions for is_offhore & get_distance_to_coast

--- scripts/topografic.py
+++ scripts/topografic.py
@@ -1,6 +1,7 @@
 import dataclasses
 import cartopy.crs as ccrs
 import geopandas as gpd
+import pandas as pd
 import rasterio
 import numpy as np
 import shapely
@@ -11,6 +12,8 @@ from PIL import Image
 from rasterio.plot import show
 from collections import namedtuple
 import atlite
+from sympy import Point, Polygon
+
 
 
 def _get_water_depth(
@@ -24,8 +27,26 @@ def _get_water_depth(
 
     water_depth = namedtuple("water_depth", ["depth_map", "depth"])
     return water_depth(depth_map=depth_map, depth=depth_map[row, col])
+ def is_location_offshore(countries = 1, point = 1) -> bool:
+     germany = countries.loc["DE"]
+     return germany.geometry.contains(point)
 
 
+def get_distance_to_coast(countries = 1, point = 1, toggle: bool = True) -> float:
+
+    germany = countries.loc["DE"]
+    denmark = countries.loc["DK"]
+    sweden = countries.loc["SE"]
+    norway = countries.loc["NO"]
+
+    if toggle == True:
+
+        distance_point = [germany.geometry.distance(point), denmark.geometry.distance(point], sweden.geometry.distance(point), norway.geometry.distance(point)]
+        min_distance = min(distance_point)
+    else:
+        min_distance = germany.geometry.distance(point)
+
+        return min_distance
 @dataclasses.dataclass
 class Location:
     x: float
@@ -35,7 +56,7 @@ class Location:
     depth_map: rasterio.Band = dataclasses.field(init=False)
     depth: float = dataclasses.field(init=False)
     ground_material: str = dataclasses.field(init=False)
-    point: shapely.Point = dataclasses.field(init=False)
+    point: shapely.geometry.Point = dataclasses.field(init=False)
 
     def __post_init__(self):
         with rasterio.open("../data/maps/GEBCO_WATER_DEPTH.tif") as dataset:
@@ -51,44 +72,6 @@ class Location:
             crs=self.projection,
             dataset=self.depth_dataset,
         ).depth_map
-
-    def is_location_offshore(self) -> bool:
-        """
-        Takes location class to return bool (True or False) whether the given location is offshore or not
-        -> Google: "Check if point is in geopandas shape" oder so ähnlich.
-            Den Geopandas dataframe (also countries bekommt man durch: self.countries
-            Den Point durch self.point (es ist ein geopandas point)
-        :return:
-        """
-        pass
-
-    def get_distance_to_coast(self) -> float:
-        """
-        Function to return float for flight distance to next German coast
-        Google: "get distance point to shape"
-            geopandas shapes (also countries findet ihr in self.countries)
-            den point wieder in self.point
-            Eventuell muss der Geopandas dataframe auf "DE" reduziert werden.
-                Am anpassungsfähigsten ist die funktion dann wenn wir einen trigger einbauen der kann ja auch default
-                auf True gesetzt werden mit dem man nur den Abstand zu Deutschladn oder eben auch zu den anderen
-                Ländern setzen kann. Um Defaults zu setzen würde die Methodendefinition sich zu
-                def get_distance_to_coast(self, only_germany:bool = True) -> float: ändern
-                Später kann dann so (wenn wir das dann wollen) diese Funktion über einen Knopf in der App angeschaltet
-                werden
-
-
-
-            Kleine Nachhilfe in annotations:
-                Sinn von annotations: Eine gute IDE (wie Pycharm) checkt ob man alles richtig macht und
-                ob die richtigen Variabletypen übergeben werden wenn alles annotiert ist vereinfacht dass
-                das coding und die Wartung des Codes.
-                -> float : gibt an was die Funktion zurückgeben soll
-                :bool gibt an dass die eingangssvariable eine bool variable ist also entweder True oder False
-
-            
-        :return:
-        """
-        pass
 
 
 
