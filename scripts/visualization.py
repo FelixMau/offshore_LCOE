b6e16723c51ed64a62e05aae98797beb5c0cdf81
FelixMau <98911066+FelixMau@users.noreply.github.com>
2023-07-11 14:30:43 +0200

    Change x, y to lon lat

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -315,6 +315,7 @@ def main():
                     },
                     inplace=True,
                 )
+                df.index.names = ["Longitude", "Latitude"]
                 st.write(df.sort_values(by="lcoe [€_MWh]").head(5))
                 st.download_button(
                     label="Download data as CSV",
